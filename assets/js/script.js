90e9d495f8ddd4ccb14eee6c3e67900de6350adc
Iopen <68678426+iopenet@users.noreply.github.com>
2021-08-20 11:49:39 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
+//Highlight code
+    hljs.initHighlightingOnLoad();
+
 // Clipboard
-var url = document.location.href;
-    new ClipboardJS('.code', {
-        text: function() {
-    return url;
-  }
-});
+    var clipboard = new ClipboardJS('.btn');
+
+    clipboard.on('success', function (e) {
+        // console.log(e);
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+        console.log(e);
+    });
 
 
 // Toastr function
 $(".code").click(function() {
-	toastr.options.positionClass = "toast-top-center";
-	toastr.success('&nbsp; &nbsp; — &nbsp; Copied to the clipboard &nbsp; —');
+    toastr.options.positionClass = "toast-top-center";
+    toastr.success('&nbsp; &nbsp; — &nbsp; Copied to the clipboard &nbsp; —');
 });
