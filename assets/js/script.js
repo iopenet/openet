2750a9f250fd588b18ee4a328f389835abc64897
Iopen <68678426+iopenet@users.noreply.github.com>
2021-08-20 10:32:38 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 
+(function(){
+    new Clipboard('.code');
+})();
+
+
+// Toastr function
+$(".code").click(function() {
+	toastr.options.positionClass = "toast-top-center";
+	toastr.success('&nbsp; &nbsp; — &nbsp; Copied to the clipboard &nbsp; —');
+});
